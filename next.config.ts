3a7fd0b36098b39562b2bed3997efbaafea02524
Андрей Грек <agrek@inno.tech>
2026-01-26 19:08:32 +0300

    github pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  // Если репозиторий называется Artway, то basePath не нужен
+  // Если репозиторий называется artwayinc.github.io, то basePath тоже не нужен
+  // Если репозиторий называется Artway и будет доступен по адресу artwayinc.github.io/Artway, то нужен basePath: '/Artway'
+  // basePath: process.env.NODE_ENV === 'production' ? '/Artway' : '',
+  trailingSlash: true,
 };
 
 export default nextConfig;
